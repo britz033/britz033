de0def11bd0fea784d669e53ee66bb72710c4aef
pc-1 <lol@lol-PC>
2013-11-16 13:49:04 +0900

    현재 경로없는게 있어서 에러 뜸

--- BusNew/src/subfragment/BusInfoActivity.java
+++ BusNew/src/subfragment/BusInfoActivity.java
@@ -29,12 +29,13 @@ public class BusInfoActivity extends FragmentActivity implements LoaderCallbacks
 	private ViewPager pathPager;
 	private ArrayList<String> path;
 	private Cursor mcursor;
-	private boolean flag = true;
+	private int loopIndex;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_businfo);
+		loopIndex = 0;
 
 		pathPager = (ViewPager) findViewById(R.id.viewpager_activity_businfo_path);
 
@@ -52,6 +53,7 @@ public class BusInfoActivity extends FragmentActivity implements LoaderCallbacks
 
 	}
 
+	// 로더 쿼리가 끝난후 호출
 	private void initBusInfo() {
 		mcursor.moveToFirst();
 
@@ -66,17 +68,12 @@ public class BusInfoActivity extends FragmentActivity implements LoaderCallbacks
 		path = new ArrayList<String>();
 		String station = null;
 		Bundle data = new Bundle();
-		
-		flag = false;
-		
+
 		while (true) {
 			if (!matcher.find())
 				break;
 			station = matcher.group(1);
 			path.add(station);
-			data.putString(KEY_PATH_STATION, station);
-			LoaderManager lm = getSupportLoaderManager();
-			lm.restartLoader(1, data, this);
 		}
 
 		PathPagerAdapter<BusInfoPathItemFragment> adapter = new PathPagerAdapter<BusInfoPathItemFragment>(
@@ -87,63 +84,81 @@ public class BusInfoActivity extends FragmentActivity implements LoaderCallbacks
 		TextView textBusInterval = (TextView) findViewById(R.id.text_activity_businfo_current);
 		textBusInterval.setText(busInterval);
 		
+		loopQuery();
+		
+	}
+	
+	public void loopQuery(){
+		Log.d("루프 스테이션 이름",path.get(loopIndex));
+		Bundle data = new Bundle();
+		data.putString(KEY_PATH_STATION, path.get(loopIndex));
+		loopIndex++;
+		getSupportLoaderManager().restartLoader(1, data, this);
+		
 	}
 
 	@Override
 	public Loader<Cursor> onCreateLoader(int id, Bundle data) {
-		
+
 		Uri uri = null;
 		String[] projection = null;
 		String selection = null;
-				
+
 		if (id == 0) {
+			Log.d("로더 init",data.getString(KEY_BUS_INFO));
 			uri = MyContentProvider.CONTENT_URI_BUS;
-			projection = new String[]{ "_id", "bus_interval", "bus_forward", "bus_backward", "bus_favorite" };
+			projection = new String[] { "_id", "bus_interval", "bus_forward", "bus_backward", "bus_favorite" };
 			selection = "bus_number='" + data.getString(KEY_BUS_INFO) + "'";
 		} else if (id == 1) {
-			
 			uri = MyContentProvider.CONTENT_URI_STATION;
-			projection = new String[]{ "_id", "station_longitude" , "station_latitude" };
+			projection = new String[] { "_id", "station_longitude", "station_latitude" };
 			selection = "station_name='" + data.getString(KEY_PATH_STATION) + "'";
-			
 		}
-		
+
 		return new CursorLoader(this, uri, projection, selection, null, null);
 	}
 
 	@Override
-	public void onLoadFinished(Loader<Cursor> arg0, Cursor cursor) {
-		if(flag){
-		mcursor = cursor;
-		initBusInfo();
-		} else {
+	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+		switch(loader.getId()){
+		case 0:
+			mcursor = cursor;
+			initBusInfo();
+			break;
+		case 1:
 			cursor.moveToNext();
-//			Log.d("좌표",String.valueOf(cursor.getDouble(1)));
+			Log.d("좌표",String.valueOf(cursor.getDouble(1)));
+			if(loopIndex < path.size()){
+				loopQuery();
+			}
+			break;
 		}
 		
+		
+
 	}
 
 	@Override
 	public void onLoaderReset(Loader<Cursor> arg0) {
+		Log.d("로더리셋","불려짐");
 	}
 
 	@Override
 	public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onPageSelected(int position) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onPageScrollStateChanged(int state) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
 }
